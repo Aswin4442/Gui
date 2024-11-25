c5e6dca1f905472fcdae07a6e38842d216dc1532
Aswin4442 <aswinbabu4442@gmail.com>
2024-11-25 11:36:34 +0530

    Add files via upload

---
+++ entry_gui.py
@@ -0,0 +1,24 @@
+# from tkinter import *
+# root=Tk()
+# label=Label(root,text="username")
+# label.pack()
+# entry=Entry(root,bg="white",fg="green",bd=5)
+# entry.pack()
+# root.mainloop()
+
+
+# from tkinter import*
+# signup=Tk()
+# label=Label(signup,text="signin",font=(("25")))
+# label.pack()
+# label1=Label(signup,text="name")
+# label1.place(x=580,y=30)
+# entry1=Entry(signup,bg="white",fg="black",bd="5")
+# entry1.pack(pady=30)
+# label1=Label(signup,text="password")
+# label1.place(x=580,y=90)
+# entry2=Entry(signup,bg="black",fg="white",bd="5")
+# entry2.pack()
+
+# signup.mainloop()
+
